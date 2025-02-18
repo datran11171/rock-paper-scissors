52622f1edf9f8808c8ec8752288670413c5160d8
Jamie Le Tran <jamietran200@gmail.com>
2025-02-18 20:13:16 +1100

    Completed rps to have a ui and added button.

--- javascript.js
+++ javascript.js
@@ -15,35 +15,77 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    winner.textContent = '';
     humanChoice = humanChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
 
     if (humanChoice === computerChoice) {
+        winner.textContent = 'It is a tie';
         return 'It is a tie';
 
     } else if (humanChoice === 'rock') {
         if (computerChoice === 'paper') {
             computerScore++;
+            updateScore();
+            winner.textContent = 'Computer wins';
             return 'Computer wins';
         } else {
             humanScore++;
+            updateScore();
+            winner.textContent = 'Human wins';
             return 'Human wins';
         }
     } else if (humanChoice === 'paper') {
         if (computerChoice === 'scissors') {
             computerScore++;
+            updateScore();
+            winner.textContent = 'Computer wins';
             return 'Computer wins';
         } else {
             humanScore++;
+            updateScore();
+            winner.textContent = 'Human wins';
             return 'Human wins';
         }
     } else if (humanChoice === 'scissors') {
         if (computerChoice === 'rock') {
             computerScore++;
+            updateScore();
+            winner.textContent = 'Computer wins';
             return 'Computer wins';
         } else {
             humanScore++;
+            updateScore();
+            winner.textContent = 'Human wins';
             return 'Human wins';
         }
     }
+}
+
+function updateScore() {
+    content.textContent = `The score is: Human ${humanScore} - Computer ${computerScore}`;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+let roundResult = '';
+
+
+var container = document.getElementById('container');
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = `The score is: Human ${humanScore} - Computer ${computerScore}`;
+
+const winner = document.createElement("div");
+winner.classList.add("winner");
+winner.textContent = ``;
+
+
+container.appendChild(content); 
+container.appendChild(winner);
+
+if (humanScore >= 5) {
+    winner.textContent = 'Human wins the game';
+} else if (computerScore >= 5) {
+    winner.textContent = 'Computer wins the game';
 }
